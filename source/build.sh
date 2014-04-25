fe9b24cbe3fc7a6c31dfc17fdd737079262a75e8
quadnix <quadnix1@gmail.com>
2014-04-25 13:32:55 -0400

    updated build script

--- source/build.sh
+++ source/build.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-clang++ core.cpp -o slang -DSL_COMPILER=\"clang++\" -DSL_DATE_COMPILED="__TIME__ + \", \" __DATE__"
+slang++ core.cpp -o slang -DSL_COMPILER=\"clang++\" -DSL_DATE_COMPILED="__TIME__ + \", \" __DATE__"
