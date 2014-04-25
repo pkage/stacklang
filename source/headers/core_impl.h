298c10125a5bb07b677075d1818e35b4cf61fc3b
quadnix <quadnix1@gmail.com>
2014-04-25 00:43:39 -0400

    began core function implementation

---
+++ source/headers/core_impl.h
@@ -0,0 +1,33 @@
+//
+// core function implementations
+// created 4/25/14
+// by patrick kage
+//
+
+
+
+#ifndef SL_CORE_IMPL
+#define SL_CORE_IMPL
+
+#include "core_io.h"
+
+namespace impl {
+	void print(std::string str) {
+		for (int c = 0; c < str.length(); c++) {
+			if (str.at(c) == '\\') {
+				if (str.at(c + 1) == 'n') {
+					io::print('\n'); c++;	
+				} else if (str.at(c + 1) == 'r') {
+					io::print('\r'); c++;
+				}
+			} else {
+				io::print(str.at(c));
+			}
+		}
+	}
+	
+
+}
+
+
+#endif
