903b4a4fa6b8f85535a85181a8726ff50e8dd7ce
quadnix <quadnix1@gmail.com>
2014-04-25 12:51:45 -0400

    added quote stripping to core_impl

--- source/headers/core_impl.h
+++ source/headers/core_impl.h
@@ -10,18 +10,21 @@
 #define SL_CORE_IMPL
 
 #include "core_io.h"
+#include "core_tools.h"
 
 namespace impl {
 	void print(std::string str) {
-		for (int c = 0; c < str.length(); c++) {
-			if (str.at(c) == '\\') {
-				if (str.at(c + 1) == 'n') {
-					io::print('\n'); c++;	
-				} else if (str.at(c + 1) == 'r') {
-					io::print('\r'); c++;
+		if (tools::strip_quotes(str)) {
+			for (int c = 0; c < str.length(); c++) {
+				if (str.at(c) == '\\') {
+					if (str.at(c + 1) == 'n') {
+						io::print('\n'); c++;	
+					} else if (str.at(c + 1) == 'r') {
+						io::print('\r'); c++;
+					}
+				} else {
+					io::print(str.at(c));
 				}
-			} else {
-				io::print(str.at(c));
 			}
 		}
 	}
